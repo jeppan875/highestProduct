04424c646aaa58d2e34458174843d19209c90e7d
Jesper Hägglund <jeppan875@gmail.com>
2021-06-02 00:32:07 +0200

    update function

--- index.js
+++ index.js
@@ -4,27 +4,25 @@ const highestProduct = (arr) => {
   if (arr.length < 4) {
     return arr.reduce((a, b) => a * b)
   }
-  // Sort by highest first
+  // Sort by biggest first
   const sortedArr = arr.sort((a, b) => b - a);
-  const size = sortedArr.length;
+  const arrLength = sortedArr.length;
 
   // Taking into account negative values
-  const lowestTwo = sortedArr[size - 1] * sortedArr[size - 2];
+  const lowestTwo = sortedArr[arrLength - 1] * sortedArr[arrLength - 2];
   const highestTwo = sortedArr[0] * sortedArr[1];
+  const secondHighest = sortedArr[1] * sortedArr[2];
+  const productOfFirst3 = sortedArr[0] * sortedArr[1] * sortedArr[2]
 
-  if (lowestTwo > highestTwo && sortedArr[0] > 0) {
+  // Lowest two are negatives that makes the highest product with 
+  // the biggest positive integer.
+  if ((lowestTwo > highestTwo && sortedArr[0] > 0) ||
+    (sortedArr[2] <= 0 && lowestTwo > 0 && sortedArr[0] >= 0) ||
+    (lowestTwo > secondHighest && productOfFirst3 > 0)) {
     return lowestTwo * sortedArr[0];
   } 
-    
-  if (sortedArr[2] <= 0 && lowestTwo > 0) {
-    if (sortedArr[0] >= 0) {
-      return lowestTwo * sortedArr[0] 
-    } else {
-      return sortedArr[0] * sortedArr[1] * sortedArr[2];
-    }
-  }
 
-  return sortedArr[0] * sortedArr[1] * sortedArr[2];
+  return productOfFirst3;
 }
 
 // Test output
@@ -35,4 +33,6 @@ console.log(highestProduct([-100, -2, -3, -88, -99, -6]));
 console.log(highestProduct([0, -100, -2, -3, -88, -99, -6]));
 console.log(highestProduct([0, 2, -100, -2, -3, -88, -99, -6]));
 console.log(highestProduct([0, 2, 3, -100, -2, -3, -88, -99, -6]));
-console.log(highestProduct([20, 60, -2,-100, -2, -3, -88, -99, -6]));
+console.log(highestProduct([20, 60, -2, -100, -2, -3, -88, -99, -6]));
+console.log(highestProduct([1000, 2, 1, -3, -4]));
+console.log(highestProduct([1000, 2, 0, -3, -4]));
